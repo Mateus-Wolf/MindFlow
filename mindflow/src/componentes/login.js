33bc3648fe628366cbfcfd23608a94d1f6a0a435
Mateus-Wolf <mateus.wolf@faculdadecesusc.edu.br>
2024-09-20 15:06:59 -0300

    Commit da tela de recuperação da senha (não funcional)
    Neste Commit, estou inserindo a tela de recuperação de senha de maneira visual

--- mindflow/src/componentes/login.js
+++ mindflow/src/componentes/login.js
@@ -2,37 +2,133 @@ import React, { useState } from 'react';
 
 const Login = ({ voltar }) => {
   const [mostrarSenha, setMostrarSenha] = useState(false);
+  const [telaEsqueciSenha, setTelaEsqueciSenha] = useState(false);
+  const [telaAtualizarSenha, setTelaAtualizarSenha] = useState(false);
+  const [codigoEnviado, setCodigoEnviado] = useState(false);
 
   const alternarVisibilidadeSenha = () => {
     setMostrarSenha(!mostrarSenha);
   };
 
+  const mostrarTelaEsqueciSenha = () => {
+    setTelaEsqueciSenha(true);
+  };
+
+  const mostrarTelaAtualizarSenha = () => {
+    setTelaAtualizarSenha(true);
+  };
+
+  const voltarParaLogin = () => {
+    setTelaEsqueciSenha(false);
+    setTelaAtualizarSenha(false);
+    setCodigoEnviado(false); // Resetar o estado ao voltar
+  };
+
+  const enviarCodigoEmail = () => {
+    setCodigoEnviado(true);
+  };
+
   return (
     <div id='conteudo'>
-      <h2 id='titulo'>Entrar na sua conta do MindFlow</h2>
-      <form>
-        <input className='form' type="text" placeholder="Email" required />
-        <div className='password-container'>
-          <input
-            className='form password-input'
-            type={mostrarSenha ? "text" : "password"}
-            placeholder="Senha"
-            required
-          />
-          <button
-            type="button"
-            className='password-toggle'
-            onClick={alternarVisibilidadeSenha}
-          >
-            <i className={`fa ${mostrarSenha ? 'fa-eye-slash' : 'fa-eye'}`}></i>
-          </button>
-        </div>
-        <div className='form-group'>
-          <a href="#" className='forgot-password'>Esqueci minha senha</a>
-        </div>
-        <button className='btns' onClick={voltar}>Voltar</button>
-        <button className='btns' type="submit">Entrar</button>
-      </form>
+      {!telaEsqueciSenha && !telaAtualizarSenha ? (
+        <>
+          <h2>Entrar na sua conta do MindFlow</h2>
+          <form>
+            <input className='form' type="text" placeholder="Email" required />
+            <div className='password-container'>
+              <input
+                className='form password-input'
+                type={mostrarSenha ? "text" : "password"}
+                placeholder="Senha"
+                required
+              />
+              <button
+                type="button"
+                className='password-toggle'
+                onClick={alternarVisibilidadeSenha}
+              >
+                <i className={`fa ${mostrarSenha ? 'fa-eye-slash' : 'fa-eye'}`}></i>
+              </button>
+            </div>
+            <div className='form-group'>
+              <a href="#" className='forgot-password' onClick={mostrarTelaEsqueciSenha}>
+                Esqueci minha senha
+              </a>
+            </div>
+            <button className='btns' onClick={voltar}>Voltar</button>
+            <button className='btns' type="submit">Enviar</button>
+          </form>
+        </>
+      ) : null}
+
+      {telaEsqueciSenha && !telaAtualizarSenha ? (
+        <>
+          <h2>Recuperar Senha</h2>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            mostrarTelaAtualizarSenha();
+          }}>
+            <div className="email-container">
+              <input className='form' type="email" placeholder="Digite seu e-mail" required />
+              <button
+                type="button"
+                className='email-send-btn'
+                onClick={enviarCodigoEmail}
+              >
+                Enviar Código
+              </button>
+            </div>
+
+            {codigoEnviado && (
+              <p className='codigo-enviado-msg'>O Código de verificação foi enviado para o seu e-mail!</p>
+            )}
+
+            <input className='form' type="text" placeholder="Digite o código de verificação" required />
+            <button className='btns' onClick={voltarParaLogin}>Voltar</button>
+            <button className='btns' type="submit">Verificar Código</button>
+          </form>
+        </>
+      ) : null}
+
+      {telaAtualizarSenha ? (
+        <>
+          <h2>Atualizar Senha</h2>
+          <form>
+            <div className='password-container'>
+              <input
+                className='form password-input'
+                type={mostrarSenha ? "text" : "password"}
+                placeholder="Nova Senha"
+                required
+              />
+              <button
+                type="button"
+                className='password-toggle'
+                onClick={alternarVisibilidadeSenha}
+              >
+                <i className={`fa ${mostrarSenha ? 'fa-eye-slash' : 'fa-eye'}`}></i>
+              </button>
+            </div>
+            <div className='password-container'>
+              <input
+                className='form password-input'
+                type={mostrarSenha ? "text" : "password"}
+                placeholder="Confirme a Nova Senha"
+                required
+              />
+              <button
+                type="button"
+                className='password-toggle'
+                onClick={alternarVisibilidadeSenha}
+              >
+                <i className={`fa ${mostrarSenha ? 'fa-eye-slash' : 'fa-eye'}`}></i>
+              </button>
+            </div>
+            <button className='btns' onClick={voltarParaLogin}>Voltar</button>
+            <button className='btns' type="submit">Atualizar Senha e Realizar Login</button>
+          </form>
+        </>
+      ) : null}
     </div>
   );
 };

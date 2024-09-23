c621508abbf77b0a90450744dc26fb4f708d665b
Mateus-Wolf <mateus.wolf@faculdadecesusc.edu.br>
2024-09-23 16:38:16 -0300

    Adição da navegação completa do header
    todos os botões do header levam para suas respectivas telas (mesmo as que não estão prontas)

--- mindflow/src/App.js
+++ mindflow/src/App.js
@@ -7,6 +7,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './componentes/telaIncial/home';
 import Home from './componentes/telaHome/home';
 import Perfil from './componentes/telaPerfil/perfil';
+import Agendamentos from './componentes/telaAgendamentos/agendamentos';
+import ListarPacientes from './componentes/telasPacientes/telaListar';
+import CadastrarPacientes from './componentes/telasPacientes/telaCadastrar';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} /> {/* Rota para Home */}
         <Route path="/perfil" element={<Perfil />} /> {/* Rota para Perfil */}
+        <Route path="/agendamentos" element={<Agendamentos />} /> {/* Rota para Perfil */}
+        <Route path="/telaCadastrar" element={<CadastrarPacientes />} /> {/* Rota para Perfil */}
+        <Route path="/telaListar" element={<ListarPacientes />} /> {/* Rota para Perfil */}
       </Routes>
     </Router>
   );

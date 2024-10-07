e1ca68b5409d10369854cfc170f13db03ebdf4e2
Mateus-Wolf <mateus.wolf@faculdadecesusc.edu.br>
2024-10-07 14:33:45 -0300

    Commit do createdatabase
    Realizando correções no código do banco de dados, tanto para a adição dos agendamentos quanto para qualidade de vida

--- mindflow-API/createDataBase.sql
+++ mindflow-API/createDataBase.sql
@@ -16,7 +16,7 @@ CREATE TABLE usuarios (
 -- Tabela de pacientes
 CREATE TABLE pacientes (
     id SERIAL PRIMARY KEY,
-    medico_id INT REFERENCES usuarios(id),
+    medico_id INT REFERENCES usuarios(id) ON DELETE SET NULL, -- Ajuste para permitir que o médico seja removido
     nome VARCHAR(100) NOT NULL,
     idade INT NOT NULL,
     cpf VARCHAR(11) UNIQUE NOT NULL,  -- Alterado para VARCHAR(11)
@@ -24,13 +24,28 @@ CREATE TABLE pacientes (
     genero VARCHAR(10) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     telefone VARCHAR(15) NOT NULL,
-    estado_civil VARCHAR(20) NOT NULL
+    estado_civil VARCHAR(20) NOT NULL,
+    usuario_id INT REFERENCES usuarios(id) ON DELETE CASCADE -- Movido para cá
+);
+
+-- Tabela de relatórios
+CREATE TABLE relatorios (
+    id SERIAL PRIMARY KEY,
+    paciente_id INT REFERENCES pacientes(id) ON DELETE CASCADE,
+    mes INT NOT NULL,
+    ano INT NOT NULL,
+    dias_com_5_estrelas INT,
+    dias_na_media INT,
+    dias_com_1_estrela INT,
+    relacao_5_estrelas_tarefas TEXT,
+    relacao_1_estrela_tarefas TEXT
 );
 
 -- Tabela de registros de humor
 CREATE TABLE registros_humor (
     id SERIAL PRIMARY KEY,
-    paciente_id INT REFERENCES pacientes(id),
+    paciente_id INT REFERENCES pacientes(id) ON DELETE CASCADE,
+    relatorio_id INT REFERENCES relatorios(id) ON DELETE CASCADE,
     data DATE NOT NULL,
     qualidade_sono INT CHECK (qualidade_sono BETWEEN 1 AND 5),
     nivel_estresse INT CHECK (nivel_estresse BETWEEN 1 AND 5),
@@ -41,22 +56,12 @@ CREATE TABLE registros_humor (
     tarefas_lazer BOOLEAN
 );
 
--- Tabela de relatórios
-CREATE TABLE relatorios (
+-- Tabela de agendamentos
+CREATE TABLE agendamentos (
     id SERIAL PRIMARY KEY,
-    paciente_id INT REFERENCES pacientes(id),
-    mes INT NOT NULL,
-    ano INT NOT NULL,
-    dias_com_5_estrelas INT,
-    dias_na_media INT,
-    dias_com_1_estrela INT,
-    relacao_5_estrelas_tarefas TEXT,
-    relacao_1_estrela_tarefas TEXT
+    paciente_id INT NOT NULL REFERENCES pacientes(id) ON DELETE CASCADE,
+    usuario_id INT NOT NULL REFERENCES usuarios(id) ON DELETE CASCADE,
+    registro_humor_id INT REFERENCES registros_humor(id) ON DELETE CASCADE,
+    data DATE NOT NULL,
+    descricao TEXT
 );
-
--- Alterações na tabela pacientes
-ALTER TABLE pacientes DROP COLUMN IF EXISTS id_medico;
-
-ALTER TABLE pacientes ADD COLUMN usuario_id INT;
-
-ALTER TABLE pacientes ADD CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON DELETE CASCADE;
